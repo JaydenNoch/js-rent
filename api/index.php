118f4cd8360099b34e77a7339c124c9d48a4f1d4
JaydenNoch <zaidanahmad005@gmail.com>
2026-06-06 19:03:14 +0700

    fix: global putenv for bootstrap cache path redirection

--- api/index.php
+++ api/index.php
@@ -1,10 +1,10 @@
 <?php
 
-// 1. Definisikan folder kustom ke /tmp secara runtime SEBELUM Laravel di-load
+// Paksa system PHP tahu kalau folder bootstrap cache dipindah ke /tmp
 if (isset($_SERVER['VERCEL_URL'])) {
-    $_ENV['APP_STORAGE_PATH'] = '/tmp/storage';
-    
-    // Buat foldernya secara fisik di /tmp jika belum ada
+    putenv('LARAVEL_BOOTSTRAP_CACHE=/tmp/bootstrap/cache');
+    $_ENV['LARAVEL_BOOTSTRAP_CACHE'] = '/tmp/bootstrap/cache';
+
     $dirs = [
         '/tmp/storage/framework/views',
         '/tmp/storage/framework/cache',
@@ -12,6 +12,7 @@ if (isset($_SERVER['VERCEL_URL'])) {
         '/tmp/storage/logs',
         '/tmp/bootstrap/cache'
     ];
+    
     foreach ($dirs as $dir) {
         if (!is_dir($dir)) {
             mkdir($dir, 0755, true);
@@ -20,19 +21,7 @@ if (isset($_SERVER['VERCEL_URL'])) {
 }
 
 require __DIR__ . '/../vendor/autoload.php';
-
-// 2. Load aplikasi Laravel
 $app = require_once __DIR__ . '/../bootstrap/app.php';
-
-// 3. Paksa aplikasi menggunakan jalur folder /tmp tersebut
-if (isset($_SERVER['VERCEL_URL'])) {
-    $app->useStoragePath('/tmp/storage');
-    
-    // Trik pamungkas memindahkan bootstrap cache path ke /tmp secara instan
-    $app->bind('path.bootstrap', function () {
-        return '/tmp/bootstrap';
-    });
-}
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,41 +1,12 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Create The Application
-|--------------------------------------------------------------------------
-|
-| The first thing we will do is create a new Laravel application instance
-| which serves as the "glue" for all the components of Laravel, and is
-| the IoC container for the system binding all of the various parts.
-|
-*/
+$app = new Illuminate\Foundation\Application(
+    $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
+);
 
-// TRICK PAMUNGKAS VERCEL: Paksa Laravel pakai Application Class custom agar tidak nulis file cache
-$app = new class($_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)) extends Illuminate\Foundation\Application {
-    public function bootstrapPath($path = '') {
-        if (isset($_SERVER['VERCEL_URL'])) {
-            return '/tmp/bootstrap' . ($path ? DIRECTORY_SEPARATOR . $path : '');
-        }
-        return parent::bootstrapPath($path);
-    }
-};
-
-// Pindahkan storage path-nya saja ke /tmp biar session & view gak eror read-only
 if (isset($_SERVER['VERCEL_URL'])) {
     $app->useStoragePath('/tmp/storage');
 }
-
-/*
-|--------------------------------------------------------------------------
-| Bind Important Interfaces
-|--------------------------------------------------------------------------
-|
-| Next, we need to bind some important interfaces into the container so
-| we will be able to resolve them when needed. The kernels serve the
-| incoming requests to this application from both the web and CLI.
-|
-*/
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
@@ -51,16 +22,5 @@ $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
     App\Exceptions\Handler::class
 );
-
-/*
-|--------------------------------------------------------------------------
-| Return The Application
-|--------------------------------------------------------------------------
-|
-| This script returns the application instance. The instance is given to
-| the calling script so we can separate the building of the instances
-| from the actual running of the application and sending responses.
-|
-*/
 
 return $app;
